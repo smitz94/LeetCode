62adccc260e31225ebef31c691b34266e196e48a
smit zaveri <smitzaveri12@gmail.com>
2019-10-27 18:47:45 +0530

    added solution

---
+++ maximum-binary-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    // max in the sub-array
+    int max(vector<int>& nums, int l, int r) {
+        int max_i = l;
+        for (int i = l; i < r; i++) {
+            if (nums[max_i] < nums[i])
+                max_i = i;
+        }
+        return max_i;
+    }
+    
+    // constructing the tree
+    TreeNode* construct(vector<int>& nums, int l, int r) {
+        if (l == r)
+            return NULL;
+        int max_i = max(nums, l, r);
+        TreeNode* root = new TreeNode(nums[max_i]);
+        root->left = construct(nums, l, max_i);
+        root->right = construct(nums, max_i + 1, r);
+        return root;
+    }
+    
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return construct(nums, 0, nums.size());
+    }
+};
